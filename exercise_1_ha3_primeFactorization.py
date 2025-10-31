8055ab8afd4755b8cef9d04162442ecc348434c0
Robert <robert.jerye@protonmail.com>
2025-10-31 18:54:51 +0100

    homework_exercise_1
    includes a working code for each exercise including the corrected version from the tutors

---
+++ exercise_1_ha3_primeFactorization.py
@@ -0,0 +1,28 @@
+s = input("Gib eine positive ganze Zahl ein: ")
+n = int(s)
+
+print("Die Primfaktorzerlegung von", n, "ist:")
+
+teiler = 2
+pfz = "" # Hier speichere ich die PFZ während der while-Schleife
+
+while n > 1:
+    k = 0
+    while n % teiler ==0:
+        k = k + 1
+        n = n // teiler
+    if k > 0:
+        if pfz != "":
+            pfz = pfz + " * "
+        if k == 1:
+            pfz = pfz + str(teiler)
+        else:
+            pfz = pfz + str(teiler) + "^" + str(k)
+    teiler = teiler + 1
+
+print(pfz)
+
+test=1
+            
+            
+
---
+++ exercise_1_ha4_kugel.py
@@ -0,0 +1,18 @@
+# Robert Jerye studonid: uz67izax
+
+import math
+
+"""
+"math" out U_K
+
+Bogensegment: l=r*alpha
+sin(alpha)=r_K/r => r_K=r*sin(alpha)
+U_K = abs(2*PI*r_K) => U_K = abs(2*PI*r*sin(l/r))
+"""
+#user input, calulations and output
+print('Radius r der Kugel[cm]: ',end='')
+r = float(input())
+print('Länge l der Schnur[cm]: ',end='')
+l = float(input())
+print('Der Kreisumfang beträgt ',end='')
+print(abs(2*math.pi*r*math.sin(l/r)),'cm',sep='')
